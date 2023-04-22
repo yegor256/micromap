ebf70606d249b0fa4aaa9932ecb10de1842165ae
Yegor Bugayenko <yegor256@gmail.com>
2023-04-22 08:00:56 +0300

    #19 test hashbrown

--- tests/benchmark.rs
+++ tests/benchmark.rs
@@ -63,6 +63,12 @@ fn benchmark(total: usize) -> HashMap<&'static str, Duration> {
         total
     );
     insert!(
+        "hashbrown::HashMap",
+        ret,
+        hashbrown::HashMap::<u32, i64>::new(),
+        total
+    );
+    insert!(
         "micromap::Map",
         ret,
         micromap::Map::<u32, i64, CAPACITY>::new(),
