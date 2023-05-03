e88a33f8601b7262d44fe950d479b514b0573a42
Sergey Sharov <mutastackk@gmail.com>
2023-05-03 23:43:13 +0600

    #10 Values, ValuesMut and IntoValues.

--- src/lib.rs
+++ src/lib.rs
@@ -57,6 +57,7 @@ mod iterators;
 mod map;
 #[cfg(feature = "serde")]
 mod serialization;
+mod values;
 
 use std::mem::MaybeUninit;
 
@@ -112,4 +113,19 @@ pub struct IterMut<'a, K, V> {
 pub struct IntoIter<K: PartialEq, V, const N: usize> {
     pos: usize,
     map: Map<K, V, N>,
+}
+
+/// An iterator over the values of the [`Map`].
+pub struct Values<'a, K: PartialEq, V, const N: usize> {
+    iter: Iter<'a, K, V, N>,
+}
+
+/// Mutable iterator over the values of the [`Map`].
+pub struct ValuesMut<'a, K: PartialEq, V> {
+    iter: IterMut<'a, K, V>,
+}
+
+/// Consuming iterator over the values of the [`Map`].
+pub struct IntoValues<K: PartialEq, V, const N: usize> {
+    iter: IntoIter<K, V, N>,
 }
