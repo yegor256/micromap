0e03cbd81f3badd267dfdcec27701104cf4c953e
owtotwo <owtotwo@163.com>
2025-04-10 17:49:02 +0800

    bench: do not touch the benchmark.rs .

--- tests/benchmark.rs
+++ tests/benchmark.rs
@@ -6,7 +6,6 @@
 
 use std::collections::HashMap;
 use std::env;
-use std::hint::black_box;
 use std::time::{Duration, Instant};
 
 const CAPACITY: usize = 1;
@@ -19,7 +18,7 @@ macro_rules! eval {
             let _ = $map.insert(0, 42);
             for i in 1..$capacity - 1 {
                 let _ = $map.insert(i as u32, i as i64);
-                let v = black_box(*$map.get(&(i as u32)).unwrap());
+                let v = std::hint::black_box(*$map.get(&(i as u32)).unwrap());
                 assert_eq!(v, i as i64);
             }
             for i in 1..$capacity - 1 {
@@ -30,9 +29,9 @@ macro_rules! eval {
             if $map.iter().find(|(_k, v)| **v == 0).is_some() {
                 $map.clear();
             }
-            sum += black_box($map.iter().find(|(_k, v)| **v == 42).unwrap().1);
+            sum += std::hint::black_box($map.iter().find(|(_k, v)| **v == 42).unwrap().1);
         }
-        black_box(sum)
+        std::hint::black_box(sum)
     }};
 }
 
